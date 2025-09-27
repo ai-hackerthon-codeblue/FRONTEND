3597350122c1cfdc474829611930dec8254b7496
BuBuck <sunjeongin123@gmail.com>
2025-09-27 21:06:26 +0900

    feat: PolyLine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import "./App.css"
+import React from 'react';
+import MapContainer from './MapContainer';
+import './App.css';
 
-const App = () => {
-
+function App() {
   return (
     <div className="App">
-
+      <header className="App-header">
+        <h1>React에서 네이버 지도 Polyline 그리기</h1>
+      </header>
+      <main>
+        <MapContainer />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
