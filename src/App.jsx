302097c206f6f42d37b9b242510454471d63c16d
BuBuck <sunjeongin123@gmail.com>
2025-09-27 21:14:55 +0900

    Revert "feat: PolyLine"
    This reverts commit 3597350122c1cfdc474829611930dec8254b7496.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import MapContainer from './MapContainer';
-import './App.css';
+import "./App.css"
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React에서 네이버 지도 Polyline 그리기</h1>
-      </header>
-      <main>
-        <MapContainer />
-      </main>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
